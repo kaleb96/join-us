fd0da1c091dec40c52d6320b0144339acc9bcf46
kaleb96 <yes505304@gmail.com>
2024-04-01 19:36:06 +0900

    #3
     [x] finished stage 1
     [x] apply name validation
     [x] apply ctn validation
     [ ] apply daum post code

--- components/SignUpInfo.vue
+++ components/SignUpInfo.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 
-//state
+//states
 const email = ref('');
 const passwd = ref('');
 const confirmPasswd = ref('');
-const errorMessage = ref('');
 const isEmailChecked = ref(true);
 const isPasswdChecked = ref(true);
 const isPasswdConfirmed = ref(true);
+const isNext = ref(true);
+const router = useRouter();
 
 //functions
 //이메일 확인
@@ -15,12 +16,12 @@ const handleEmail = () => {
     
     if(email.value) {
 
-        if(!validateEmail(email.value)) { //불일치
+        if(!validateEmail(email.value)) { //fail
             return isEmailChecked.value = false;
         } 
     } 
 
-    return isEmailChecked.value = true; //일치
+    return isEmailChecked.value = true; //success
 }
 
 //이메일 유효성 검사
@@ -40,12 +41,11 @@ const handlePasswd = () => {
         let reg = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 
         if(!reg.test(String(passwd.value))) { // 비밀번호 유효성 검사
-            //불일치
-            errorMessage.value = '비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.';
-            return isPasswdChecked.value = false
+           
+            return isPasswdChecked.value = false //fail
         }         
     }
-    return isPasswdChecked.value = true; //일치
+    return isPasswdChecked.value = true; //success
 }
 
 //비밀번호 재확인 검사
@@ -53,12 +53,27 @@ const handleConfirm = () => {
 
     if(confirmPasswd.value) {
 
-        if(passwd.value != confirmPasswd.value) { // 불일치
-            return isPasswdConfirmed.value = false;
+        if(passwd.value != confirmPasswd.value) { 
+            return isPasswdConfirmed.value = false; //fail
         } 
     }
-    return isPasswdConfirmed.value = true; //일치
+    return isPasswdConfirmed.value = true; //success
 }
+
+//다음버튼 체크
+const handleNext = () => {
+
+    if(isEmailChecked.value && isPasswdChecked.value && isPasswdConfirmed.value) {
+
+        isNext.value = true; //success
+        // console.log('all true');
+        router.push('/DeliveryInfo');
+    } else {
+        // console.log('아님 ' + { isEmailChecked, isPasswdChecked, isPasswdConfirmed});
+        isNext.value = false; //fail
+    }
+}
+
 </script>
 
 <template>
@@ -67,21 +82,20 @@ const handleConfirm = () => {
             <div>
                 <span>이메일</span>
                 <a-input type="text" v-model:value="email" @change="handleEmail"></a-input>
-                <a-input v-if="!isEmailChecked" status="error" placeholder="정상적인 이메일 주소가 아닙니다."></a-input>
+                <a-input v-if="!isEmailChecked" status="error" placeholder="정상적인 이메일 주소가 아닙니다." disabled></a-input>
             </div>
             <div>
                 <span>비밀번호</span>
                 <a-input type="password" v-model:value="passwd" @change="handlePasswd"></a-input>
-                <a-input v-if="!isPasswdChecked" status="error" placeholder="비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다."></a-input>
+                <a-input v-if="!isPasswdChecked" status="error" placeholder="비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다." disabled></a-input>
             </div>
             <div>
                 <span>비밀번호 확인 </span>
                 <a-input type="password" v-model:value="confirmPasswd" @change="handleConfirm"></a-input>
-                <a-input v-if="!isPasswdConfirmed" status="error" placeholder="비밀번호를 다시 확인해 주세요."></a-input>
+                <a-input v-if="!isPasswdConfirmed" status="error" placeholder="비밀번호를 다시 확인해 주세요." disabled></a-input>
             </div>
-            <a-button type="primary">
-                <NuxtLink to="/DeliveryInfo">다음</NuxtLink>
-            </a-button>
-        </a-space>
+            <a-button type="primary" @click="handleNext">다음</a-button>
+            <a-input v-if="!isNext" status="error" placeholder="입력한 정보를 확인해주세요" disabled></a-input>
+        </a-space> 
     </div>
 </template>
--- pages/DeliveryInfo.vue
+++ pages/DeliveryInfo.vue
@@ -1,20 +1,66 @@
+<script setup lang="ts">
+
+//states
+const name = ref('');
+const ctn = ref('');
+const zonecode = ref('');
+const detailAddress = ref('');
+const isNameChecked = ref(true);
+const isCtnChecked = ref(true);
+const isScriptLoaded = ref(false);
+//functions
+//이름 유효성 검사
+const handleName = () => {
+
+    if(name.value) {
+
+        let reg = /^(?:[가-힣]{2,}|[a-zA-Z]{3,})$/;
+        
+        if(!reg.test(String(name.value))) { //fail
+            return isNameChecked.value = false
+        } 
+    }
+    return isNameChecked.value = true; //success
+}
+
+//연락처 유호성 검사
+const handleCtn = () => {
+
+    if(ctn.value) {
+
+        let reg = /^0\d{2}[ -]?\d{3,4}[ -]?\d{4}$/;
+        // console.log('test is = ' + reg.test(String(ctn.value)))
+
+        if(!(reg.test(String(ctn.value)))) { //false
+            return isCtnChecked.value = false;
+        }
+    } 
+
+    return isCtnChecked.value = true; //success
+}
+
+
+</script>
+
 <template>
     <div>
         <a-space direction="vertical">
             <div>
                 <span>이름</span>
-                <a-input type="text"></a-input>
+                <a-input type="text" v-model:value="name" @change="handleName"></a-input>
+                <a-input v-if="!isNameChecked" placeholder="이름을 다시 확인 해주세요" disabled></a-input>
             </div>
             <div>
                 <span>연락처</span>
-                <a-input type="text"></a-input>
+                <a-input type="text" v-model:value="ctn" @change="handleCtn"></a-input>
+                <a-input v-if="!isCtnChecked" placeholder="연락처를 다시 확인 해주세요" disabled></a-input>
             </div>
             <div>
                 <span>주소</span>
-                <a-button type="primary">우편번호</a-button>
+                <button>우편번호</button>
+                <a-input type="text" readonly v-model:value="zonecode"></a-input>
+                <a-input type="text" readonly v-model:value="detailAddress"></a-input>
             </div>
-            <a-input></a-input>
-            <a-input></a-input>
             <div>
                 <a-button>
                     <NuxtLink to="/">이전</NuxtLink>
